2d0fa3ae1874e5a9a20c84489b073819547cea26
Nicolas Verón <nveronpf@gmail.com>
2022-08-20 14:53:06 -0300

    Base

--- src/componentes/imagenes.js
+++ src/componentes/imagenes.js
@@ -1,13 +1,13 @@
 import cheeseburger from './imagenes/cheeseburger.jpg';
-// import cheesebaconburger from './imagenes/cheesebaconburger.jpg';
-// import crispyburger from './imagenes/crispyburger.jpg';
-// import onionburger from './imagenes/onionburger.jpg';
-// import veggieburger from './imagenes/veggieburger.jpg';
+import cheesebaconburger from './imagenes/cheesebaconburger.jpg';
+import crispyburger from './imagenes/crispyburger.jpg';
+import onionburger from './imagenes/onionburger.jpg';
+import veggieburger from './imagenes/veggieburger.jpg';
 
 export default {
     'cheeseburger': cheeseburger,
-    // 'cheesebaconburger': cheesebaconburger,
-    // 'crispyburger': crispyburger,
-    // 'onionburger': onionburger,
-    // 'veggieburger': veggieburger,
+    'cheesebaconburger': cheesebaconburger,
+    'crispyburger': crispyburger,
+    'onionburger': onionburger,
+    'veggieburger': veggieburger,
 }
