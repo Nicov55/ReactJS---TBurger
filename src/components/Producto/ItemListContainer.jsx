fc78a61e0b7ee91a46be6285a44f8e500c891604
Nicolas Verón <nveronpf@gmail.com>
2022-09-05 19:42:55 -0300

    Actualizando

--- src/components/Producto/ItemListContainer.jsx
+++ src/components/Producto/ItemListContainer.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import ItemCount from '../Botones/ItemCount';
 
-const ItemListContainer = (greetings) => {
+const ItemListContainer = (props) => {
 
   const onAdd = (cantidad) => {
-    console.log(`Compraste ${cantidad} unidades de ${greetings.nombre}`);
+    console.log(`Compraste ${cantidad} unidades de ${props.nombre}`);
   }
 
   return (
 
 <div className="col mb-5">
-  <div className="card h-100" key={greetings.id}>
+  <div className="card h-100" key={props.id}>
     
     {/* IMAGEN */}
 
-      <img className="card-img-top imgburger" id="imgburger-0" src={greetings.imagen} alt="..." />
+      <img className="card-img-top imgburger" id="imgburger-0" src={props.imagen} alt="..." />
 
           <div className="card-body p-4">
               <div className="text-center">
 
     {/* NOMBRE DEL PRODUCTO */}
 
-              <h5 className="nombreburger fw-bolder" >{greetings.nombre}</h5>
+              <h5 className="nombreburger fw-bolder" >{props.nombre}</h5>
 
     {/* PRECIO */}
-                    <p className="precio" id="precio-0">$ {greetings.precio}</p>
+                    <p className="precio" id="precio-0">$ {props.precio}</p>
 
 
     {/* ABRO DETALLES */}
                     
                     <details>
                         <summary>Ingredientes</summary>  
-                            <p className="descripcion" id="descripcion-0">{greetings.descripcion}</p>
+                            <p className="descripcion" id="descripcion-0">{props.descripcion}</p>
                     </details>
                     </div>
                     </div>
