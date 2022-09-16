c79ed57f77f1d04668684391d3366ea31cb9a028
Nicolas Verón <nveronpf@gmail.com>
2022-09-16 17:29:58 -0300

    Sincronizando Contador

--- src/components/Producto/Detalles/ItemDetail.jsx
+++ src/components/Producto/Detalles/ItemDetail.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import '../../../assets/css/style.css';
 import ItemCount from '../../Botones/ItemCount';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 
 const ItemDetail = ({item}) => {
- 
+
+  const [goToCart,setGoToCart] = useState (false)
+
   const onAdd = (cantidad) => {
-    console.log(`Compraste ${cantidad} unidades de ${item.nombre}`);
+    setGoToCart(true);
   };
 
   return (
-<div className="card mb-3 detailcard">
-  <div className="row g-0">
-    <div className="col-md-6">
-      <img src={item.imagen}className="img-fluid rounded-start detailimg" alt="..." />
-    </div>
-          <div className="col-md-6">
-            <div className="card-body">
-              <br />
-              <h4 className="card-title card-header">{item.nombre}</h4>
-              <p className="card-text card-body">{item.descripcion}</p>
-              <p className="card-text card-footer fw-bold text-end">${item.precio}</p>
-            <div className="text-end">
-            <ItemCount initial={1} stock={8} onAdd= {onAdd} />
+    <div className="card mb-3 detailcard">
+      <div className="row g-0">
+        <div className="col-md-6">
+          <img src={item.imagen}className="img-fluid rounded-start detailimg" alt="..." />
+        </div>
+              <div className="col-md-6">
+                <div className="card-body">
+                  <br />
+                  <h4 className="card-title card-header">{item.nombre}</h4>
+                  <p className="card-text card-body">{item.descripcion}</p>
+                  <p className="card-text card-footer fw-bold text-end">${item.precio}</p>
+                <div className="text-end">
+                {
+                  goToCart 
+                  ? <Link className = "btn btn-outline-success" role="button" to="/cart">Finalizar Pedido</Link>
+                  : <ItemCount initial={1} stock={8} onAdd= {onAdd} />
+                }
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
   )
 }
 
